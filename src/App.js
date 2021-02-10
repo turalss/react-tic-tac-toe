b655d0ab69fed1d54236fba79c38a5318775d3d7
Tural Hasanli <tural.hasanli@smoothstack.com>
2021-02-10 06:01:23 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Game from './components/Game'
+
+function App() {
+  return (
+    <Game />
+  );
+}
+
+export default App;
